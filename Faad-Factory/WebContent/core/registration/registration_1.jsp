28789d1a603e70eb734bbd9916ed59683f29a893
maany <imptodefeat@gmail.com>
2014-04-07 16:02:06 +0530

    registration layout
    Signed-off-by: maany <imptodefeat@gmail.com>

---
+++ Faad-Factory/WebContent/core/registration/registration_1.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@taglib uri="/struts-tags" prefix = "s" %>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<s:form action="register_1" method="post">
+<s:textfield name="firstName" label="First Name "/>
+<s:textfield name="lastName" label="Last Name "/>
+<s:textfield name="eMail" label="E-Mail "/>
+<s:submit value="Next"/>
+</s:form>
+</body>
+</html>
--- Faad-Factory/src/com/app/core/actions/Registration.java
+++ Faad-Factory/src/com/app/core/actions/Registration.java
@@ -1,5 +1,43 @@
 package com.app.core.actions;
 
-public class Registration {
+import com.opensymphony.xwork2.ActionSupport;
 
+public class Registration extends ActionSupport{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String firstName,lastName,eMail;
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	
+	public String getLastName()
+	{
+		return lastName;
+	}
+	public String getEMail()
+	{
+		return eMail;
+	}
+	
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName)
+	{
+		this.lastName=lastName;
+	}
+	public void setEMail(String eMail)
+	{
+		this.eMail=eMail;
+	}
+	public String r1()
+	{
+		
+		System.out.print(firstName);//run validations
+		return SUCCESS;
+	}
 }
