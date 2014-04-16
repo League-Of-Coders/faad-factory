2e5565f8cb8e0da8b38aacd16ef0dab56292fb0b
maany <imptodefeat@gmail.com>
2014-04-16 16:30:10 +0530

    hibernate issue starting with profile

--- Faad-Factory/src/com/app/core/registration/actions/Registration5.java
+++ Faad-Factory/src/com/app/core/registration/actions/Registration5.java
@@ -8,15 +8,18 @@ import org.apache.struts2.interceptor.ApplicationAware;
 
 
 
+import org.apache.struts2.interceptor.SessionAware;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import com.app.core.AppEngine;
 import com.app.core.models.User;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class Registration5 extends ActionSupport implements ApplicationAware{
+public class Registration5 extends ActionSupport implements ApplicationAware,SessionAware{
 	private String id;
 	private Map<String,Object> application;
+	private Map<String,Object> faadSession;
 	private Session session;
 	public String getId()
 	{
@@ -27,7 +30,10 @@ public class Registration5 extends ActionSupport implements ApplicationAware{
 		this.id=id;
 	}
 	/*
-	 * Get user from application scope based on id and set it active
+	 * Get user from application scope based on id and set it active, make database record and set session, redirect to profile
+	 */
+	/**
+	 * @return
 	 */
 	public String r5()
 	{
@@ -39,28 +45,53 @@ public class Registration5 extends ActionSupport implements ApplicationAware{
 		/*
 		 * create a record in database, redirect to profile
 		 */
-		try{
+		Transaction tx = null ;
 			user.setActive(true);
 			System.out.println(user.getFirstName() + " is now " + user.getActive() + "\nwriting to database");
 			session = AppEngine.getInstance().getHibernateSession();
-			session.beginTransaction();
-			session.persist(user);
-			session.getTransaction().commit();
-			application.remove(user);
-			return SUCCESS;
-		}catch(Exception ex)
-		{
-			System.out.println("error from r5 :" + ex.getMessage());
-			return "error";
-		}
-		finally {
-			session.close();
+			System.out.println(user.getUserName() + " has widgets " + user.getWidgets());
 			
-		}
+			try {
+				tx = session.beginTransaction();
+				// TODO not working session.persist(user);
+				//session.flush();
+				tx.commit();
+			
+			}catch(Exception ex)
+			{
+				tx.rollback();
+				throw ex;
+			}finally {
+				session.close();
+				faadSession.put("user",user);
+				application.remove(user);
+						
+			}
+			return SUCCESS;
+			/*user=null;
+			session = AppEngine.getInstance().getHibernateSession();
+			session.beginTransaction();
+			System.out.println("getting user from database");
+			user=(User)session.get(User.class, 1);
+			System.out.println(user.getFirstName());
+			session.getTransaction().commit();
+			session.close();*/
+			
+			/*System.out.println("Hibernate Session closed");
+			//application.remove(user);
+			//System.out.println("User removed from Application Scope");
+			return SUCCESS; */
+			
+		
 	}
 	@Override
 	public void setApplication(Map<String, Object> arg0) {
 		application = arg0;
+		
+	}
+	@Override
+	public void setSession(Map<String, Object> arg0) {
+		this.faadSession = arg0;
 		
 	}
 }
